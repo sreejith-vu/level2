dd5e96eb11796654162ec663a102fc2fdb4e7017
Sreejith <38622608+sreejith-vu@users.noreply.github.com>
2019-08-10 20:32:16 +0530

    Update tag-and-push-to-repo.sh

--- tag-and-push-to-repo.sh
+++ tag-and-push-to-repo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-DOCKERHUB_USERNAME
-DOCKER_REPO
-
 TAG=$(cat .env  |grep TAG |awk -F= '{print $NF}')
+BACKEND_APP=$(cat .env |grep BACKEND_APP |awk -F= '{print $NF}')
+DOCKER_REPO=$(cat .env |grep DOCKER_REPO |awk -F= '{print $NF}')
+DOCKERHUB_USER=$(cat .env |grep DOCKERHUB_USER |awk -F= '{print $NF}')
 
-docker tag email-app:$TAG sreejithvu/kube-public-repo:$TAG
+docker tag $BACKEND_APP:$TAG $DOCKERHUB_USER/$DOCKER_REPO:$TAG
+docker push $DOCKERHUB_USER/$DOCKER_REPO:$TAG
