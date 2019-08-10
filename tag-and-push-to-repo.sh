599291227fbdd3293ac78883b2374d9b9175b261
Sreejith <38622608+sreejith-vu@users.noreply.github.com>
2019-08-10 23:13:51 +0530

    Update tag-and-push-to-repo.sh

--- tag-and-push-to-repo.sh
+++ tag-and-push-to-repo.sh
@@ -12,3 +12,8 @@ docker push $DOCKERHUB_USER/$DOCKER_REPO:$TAG
 sed -i "s/TAG/$TAG/g" deployments/email-app-deployment.yaml
 sed -i "s/DOCKER_REPO/$DOCKER_REPO/g" deployments/email-app-deployment.yaml
 sed -i "s/DOCKERHUB_USER/$DOCKERHUB_USER/g" deployments/email-app-deployment.yaml
+
+cp deployments/web-domain-ingress.yaml my-app/templates/ingress.yaml
+cp deployments/email-app-deployment.yaml my-app/templates/deployment.yaml
+cp deployments/email-app-service.yaml my-app/templates/service.yaml
+
