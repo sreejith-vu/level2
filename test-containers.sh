414520442f39ad6a9ce26e36cc593022e544e253
Sreejith <38622608+sreejith-vu@users.noreply.github.com>
2019-08-10 17:02:42 +0530

    Update test-containers.sh

--- test-containers.sh
+++ test-containers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $(docker exec -it backend-app sh -c "curl -I localhost:5000" |grep -c UNAUTHORIZED) -ne 1 ]]
+if [[ $(sudo docker exec -it backend-app sh -c "curl -I localhost:5000" |grep -c UNAUTHORIZED) -ne 1 ]]
 then
   exit 2
 fi
